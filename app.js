2ecd5fe2a1b663de061dff716104cb67ca432727
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-04-06 01:16:40 -0700

    added questions for team mates

--- app.js
+++ app.js
@@ -20,3 +20,50 @@ const createTeammanager = [
         message: 'What is the team managers phone number?',
     }
 ]
+
+const engineer = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the engineers name?',
+    },
+    {
+        type: 'input',
+        name: 'Employee ID', 
+        message: 'What is the engineers Employee ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the engineers email?',
+    },
+    {
+        type: 'input',
+        name: 'Github', 
+        message: 'What is the engineers Github username?',
+    },
+
+]
+
+const inter = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the inters name?',
+    },
+    {
+        type: 'input',
+        name: 'Employee ID',
+        message: 'What is the inters employee ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the inters email?',
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'Where does the inter go to school?',
+    },
+]
