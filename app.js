bea0c9f5bdb2ab20f6a897f6ba6d8fdbcb39f6dd
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-04-06 00:34:50 -0700

    created questions for team manager

---
+++ app.js
@@ -0,0 +1,22 @@
+const createTeammanager = [
+    {
+        type:'input',
+        name: 'name',
+        message: 'What is the team manager name?',
+    },
+    {
+        type: 'input',
+        name: 'Employee ID',
+        message: 'What is the team managers Employee ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the team managers email?',
+    },
+    {
+        type: 'input',
+        name: 'office number',
+        message: 'What is the team managers phone number?',
+    }
+]
