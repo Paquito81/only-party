c6ebbf74a09ec292dae43e4dc79582883193ea52
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-04-15 03:12:40 -0700

    troubleshooting

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -15,6 +15,18 @@ function generateHTML(team) {
     <body>
         <header class="justify-content-center bg-warning text-light flex-row">
         <h1>Super Team</h1>
+        </header>
+
+        <main>
+
+        <div class="card border-warning mb-3" style="max-width: 18rem;">
+        <div class="card-header">${team[0].role}</div>
+       <div class="card-body">
+       <h5 class="card-title">Warning card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+</div>
+        <div class=
         <h2>team manager: ${team[0].name}</h2>
     </body>    
         `
